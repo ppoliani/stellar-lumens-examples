41a034cb243cc92b8006b00ac4354fda9c715e48
ppoliani <ppoliani@gmail.com>
2018-01-15 14:50:32 +0000

    add a start script

--- src/bin/index.js
+++ src/bin/index.js
@@ -3,4 +3,11 @@ if(process.env.NODE_ENV === 'development') {
   require('dotenv').config({silent: true});
 }
 
+const {createKeypair, getPrivateKeyFromPair, getPubKeyFromPair} = require('../keyPair');
 
+const pair = createKeypair();
+const privKey = getPrivateKeyFromPair(pair);
+const pubKey = getPubKeyFromPair(pair);
+
+console.log(`Private Key (seed): ${privKey}`);
+console.log(`Public Key: ${pubKey}`);
--- src/keyPair.js
+++ src/keyPair.js
@@ -3,3 +3,9 @@ const StellarSdk = require('stellar-sdk');
 const createKeypair = () => StellarSdk.Keypair.random();
 const getPrivateKeyFromPair = pair => pair.secret();
 const getPubKeyFromPair = pair => pair.publicKey();
+
+module.exports = {
+  createKeypair,
+  getPrivateKeyFromPair,
+  getPubKeyFromPair
+};
